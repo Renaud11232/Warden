1879e42e2e02265bf4eabd93c6265d7d2d587e24
Renaud Gaspard <gaspardrenaud@hotmail.com>
2021-11-18 09:17:21 +0100

    Adds empty eye task

---
+++ app/be/renaud11232/warden/tasks/EyeTaskModule.java
@@ -0,0 +1,12 @@
+package be.renaud11232.warden.tasks;
+
+import com.google.inject.AbstractModule;
+
+public class EyeTaskModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+    }
+
+}
